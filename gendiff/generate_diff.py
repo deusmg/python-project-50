8ec0ff974780a0e07f5fc610a96a3240cc7440de
deusmg <laptevap@gmail.com>
2023-09-12 19:33:21 +0300

    add json generate_diff

---
+++ gendiff/generate_diff.py
@@ -0,0 +1,30 @@
+import json
+import difflib
+
+def generate_diff(file1_path, file2_path):
+    with open(file1_path, 'r') as file1, open(file2_path, 'r') as file2:
+        data1 = json.load(file1)
+        data2 = json.load(file2)
+
+    differ = difflib.Differ()
+    data1_str = json.dumps(data1, indent=2, sort_keys=True, separators=(',', ': ')).splitlines()
+    data2_str = json.dumps(data2, indent=2, sort_keys=True, separators=(',', ': ')).splitlines()
+    diff = list(differ.compare(data1_str, data2_str))
+
+    result = []
+    for line in diff:
+        if line.startswith('- '):
+            line = line[2:]
+            line = line.replace('"', '')
+            result.append(f'-{line}')
+        elif line.startswith('+ '):
+            line = line[2:]
+            line = line.replace('"', '')
+            result.append(f'+{line}')
+        elif line.startswith('  '):
+            line = line[1:]
+            line = line.replace('"', '')
+            result.append(f'{line}')
+    
+    return "\n".join(result)
+
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from gendiff import generate_diff
 
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
@@ -9,7 +10,13 @@ def main():
     parser.add_argument('-f', '--format', type=str, default='stylish', help='set format of output')
 
     args = parser.parse_args()
+    
+    file_path1 = args.first_file
+    file_path2 = args.second_file
+    output_format = args.format
 
+    diff = generate_diff(file_path1, file_path2)
+    print(diff)
 
 if __name__ == "__main__":
     main()
