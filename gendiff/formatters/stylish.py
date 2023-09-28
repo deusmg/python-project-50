a80a7a0b091cd26ea0188ec2b417a2f7aa22f449
deusmg <laptevap@gmail.com>
2023-09-29 02:55:08 +0300

    edit stylesh

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -6,9 +6,9 @@ INDENT = '    '
 def value_to_str(value, depth):
     if isinstance(value, dict):
         result = []
-        for key, val in value.items():
+        for key, value in value.items():
             space = INDENT * (depth + 1)
-            result.append(f"\n{space}{key}: {value_to_str(val, depth + 1)}")
+            result.append(f"\n{space}{key}: {value_to_str(value, depth + 1)}")
         line = itertools.chain('{', result, '\n', [INDENT * depth, '}'])
         return ''.join(line)
     if isinstance(value, bool):
@@ -19,44 +19,31 @@ def value_to_str(value, depth):
 
 
 def build_line(data, key, depth, INDENT='  '):
-    return f"{'  ' * depth}{INDENT}{key}: {value_to_str(data[key], depth + 1)}"
-
-
-def format_nested(key, value, depth):
-    space = REPLACER * (depth + 1)
-    nested_lines = walk(value['value'], depth + 1)
-    return f"{space * 2}{key}: {nested_lines}"
-
-
-def format_changed(key, value, depth):
-    space = REPLACER * (depth + 1)
-    old_line = build_line(value, 'old', depth, '- ')
-    new_line = build_line(value, 'new', depth, '+ ')
-    return f"{space}{old_line}\n{space}{new_line}"
-
-
-def format_removed(key, value, depth):
-    space = REPLACER * (depth + 1)
-    return f"{space}{build_line(value, 'value', depth, '- ')}"
-
-
-def format_added(key, value, depth):
-    space = REPLACER * (depth + 1)
-    return f"{space}{build_line(value, 'value', depth, '+ ')}"
-
-
-def format_unchanged(key, value, depth):
-    space = REPLACER * (depth + 1)
-    return f"{space}{build_line(value, 'value', depth)}"
+    return f"{'  ' * depth}{INDENT}{data['key']}: " \
+           f"{value_to_str(data[key], depth + 1)}"
 
 
 def walk(node, depth=0):
     lines = []
+    space = REPLACER * (depth + 1)
     for value in node.values():
-        operation = value['operation']
-        formatter = globals().get(f'format_{operation}')
-        if formatter:
-            lines.append(formatter(value['key'], value, depth))
+        if value['operation'] == 'nested':
+            lines.append(f"{space * 2}{value['key']}: "
+                         f"{walk(value['value'], depth + 1)}")
+            continue
+        if value['operation'] == 'changed':
+            lines.append(f"{space}{build_line(value, 'old', depth, '- ')}")
+            lines.append(f"{space}{build_line(value, 'new', depth, '+ ')}")
+            continue
+        if value['operation'] == 'removed':
+            lines.append(f"{space}{build_line(value, 'value', depth, '- ')}")
+            continue
+        if value['operation'] == 'added':
+            lines.append(f"{space}{build_line(value, 'value', depth, '+ ')}")
+            continue
+        if value['operation'] == 'unchanged':
+            lines.append(f"{space}{build_line(value, 'value', depth)}")
+            continue
     result = itertools.chain('{', lines, [INDENT * depth + '}'])
     return "\n".join(result)
 
